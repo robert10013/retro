41f951ee3bf8f064f94a2ace239fe57136f573e7
robert <robeethomas95@gmail.com>
2026-06-18 11:23:42 +0530

    python task

---
+++ python task 9.py
@@ -0,0 +1,12 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 1.Create a class named Bottle:
+... class Bottle:
+...     def __init__(self,color,capacity,status):
+...         self.color=color
+...         self.capacity=capacity
+...         self.status=status
+... 
+...     def open(self):
+...         self.status="opened"
+...     def close(self):
